93411a8fc549a0cb899e154d874a1066d71b6c4f
harriet-the-spy <dpcairns@gmail.com>
2021-02-19 13:34:33 -0800

    initial

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const animals = require('./animals.js');
+const candies = require('./candies.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -25,12 +25,18 @@ async function run() {
     const user = users[0].rows[0];
 
     await Promise.all(
-      animals.map(animal => {
+      candies.map(candy => {
         return client.query(`
-                    INSERT INTO animals (name, cool_factor, owner_id)
-                    VALUES ($1, $2, $3);
+                    INSERT INTO candies (name, yumminess, has_chocolate, category, owner_id)
+                    VALUES ($1, $2, $3, $4, $5);
                 `,
-        [animal.name, animal.cool_factor, user.id]);
+        [
+          candy.name, 
+          candy.yumminess, 
+          candy.has_chocolate,
+          candy.category,
+          user.id,
+        ]);
       })
     );
     
